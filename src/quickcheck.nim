61b23bae951502453cb05763b2b5dc7473483d92
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-25 13:22:09 -0300

    Add nicer error message

--- src/quickcheck.nim
+++ src/quickcheck.nim
@@ -7,15 +7,17 @@ import typetraits
 template arbitrary*(T: typedesc): auto =
   var result: T
   when compiles(rand(T)):
+    # Matches integers, etc.
     result = rand(T)
   elif compiles(rand(low(T)..high(T))):
+    # Matches floats, etc.
     result = rand(low(T)..high(T))
   elif compiles(elementType(result)):
+    # Matches sequences, strings, etc.
     for _ in 0'u8..<arbitrary(uint8):
       result.add arbitrary(elementType(result))
   else:
-    raise newException(ValueError, "cannot generate arbitrary " & $T)
-  # debugEcho result
+    raise newException(ValueError, "could not generate arbitrary " & $T & ", please open an issue at https://github.com/schneiderfelipe/quickcheck/issues/new")
   result
 
 template arbitrary*(T: typedesc[array]): auto =
