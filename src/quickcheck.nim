47da34a6fbda8de081d44c55a0f1b579e39ebdf2
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-25 19:44:44 -0300

    Add support for two-argument functions

--- src/quickcheck.nim
+++ src/quickcheck.nim
@@ -8,15 +8,16 @@ export arbitrary
 
 const prefix = "  "
 
-proc failed[T](i: int, x: T, raised = false) =
+proc failed(i: int, xs: tuple, raised = false) =
   let headline = if not raised: "Failed" else: "Raised"
   let s = if i > 1: "s" else: ""
   stdout.styledWriteLine(styleBright, fgMagenta, prefix, "*** ", resetStyle,
       &"{headline}! Falsifiable (after {i} test{s}):")
 
-  var line: string = prefix & prefix
-  line.addQuoted(x)
-  echo line
+  for x in xs.fields:
+    var line: string = prefix & prefix
+    line.addQuoted(x)
+    echo line
 
 proc succeeded(n: int) =
   stdout.styledWriteLine(styleBright, fgCyan, prefix, "+++ ", resetStyle,
@@ -27,7 +28,7 @@ proc quick*[T](f: T -> bool): bool =
     try:
       f(x)
     except:
-      failed(i, x, raised = true)
+      failed(i, (x,), raised = true)
       raise
 
   var x: T
@@ -35,7 +36,27 @@ proc quick*[T](f: T -> bool): bool =
   for i in 1..n:
     x = arbitrary(T)
     if not test(i, x):
-      failed(i, x)
+      failed(i, (x,))
+      return false
+  succeeded(n)
+  return true
+
+proc quick*[S, T](f: (S, T) -> bool): bool =
+  proc test(i: int, x: S, y: T): bool =
+    try:
+      f(x, y)
+    except:
+      failed(i, (x, y), raised = true)
+      raise
+
+  var x: S
+  var y: T
+  const n = 100
+  for i in 1..n:
+    x = arbitrary(S)
+    y = arbitrary(T)
+    if not test(i, x, y):
+      failed(i, (x, y))
       return false
   succeeded(n)
   return true
