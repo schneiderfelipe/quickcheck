10dff7be6854540fdbed802884a0e8521fac9a57
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-26 12:45:41 -0300

    Add small annotations to the code

--- src/quickcheck.nim
+++ src/quickcheck.nim
@@ -1,3 +1,23 @@
+# TODO: support shrinking (see e.g.,
+# https://raw.githubusercontent.com/leepike/SmartCheck/master/paper/paper.pdf
+# and
+# https://hackage.haskell.org/package/QuickCheck-2.14.2/docs/Test-QuickCheck.html#v:genericShrink)
+
+# TODO: support smallcheck-like checks as well (i.e., exaustiveness up to a
+# number of tests instead of random ones): "If a program fails to meet its
+# specification in some cases, it almost always fails in some simple case."
+
+# TODO: support control over tests inside the tests themselves by returning
+# an Option[bool]: if the value is some(), use it for the test, if it's
+# none(bool), ignore. Filtering can be supported on top of that (idea: by
+# using templates that force returning none(bool)).
+
+# TODO: long-shot: support forAll, exists and existsUnique (currently we only
+# envision the equivalent of forAll, see
+# https://hackage.haskell.org/package/smallcheck-1.2.1/docs/Test-SmallCheck.html#g:1)
+
+# TODO: other excellent ideas from SmallCheck: `over`, `==>`.
+
 import
   strformat,
   sugar,
@@ -8,12 +28,42 @@ export arbitrary
 
 const prefix = "  "
 
+# TODO: receive some kind of Reason object that stores the reasons of the
+# failure.
 proc failed(i: int, xs: tuple, raised = false) =
   let
     headline = if not raised: "Failed" else: "Raised"
     s = if i > 1: "s" else: ""
+  # TODO: I think the output in case of error is fine, but we need shrinking.
   stdout.styledWriteLine(styleBright, fgMagenta, prefix, "*** ", resetStyle,
-      &"{headline}! Falsifiable (after {i} test{s}):")
+      &"{headline}! Falsifiable (after {i} quick test{s}):")
+
+  # TODO: print something like:
+  #
+  # there exists [0,1] such that
+  #   condition is false
+  #
+  # or
+  #
+  # there exists [0,1] such that
+  #   reverse x = [1,0]
+  #
+  # or
+  #
+  # Failed test no. 4.
+  # there exists 4 such that
+  #   condition is false
+  #
+  # or
+  #
+  # Failed test no. 12.
+  # there are at least two arguments satisfying the property:
+  #   for 0
+  #     condition is true
+  #   for 1
+  #     property is vacuously true because
+  #       there exists -1 such that
+  #         condition is false
 
   for x in xs.fields:
     var line: string = prefix & prefix
@@ -24,14 +74,23 @@ proc failed(i: int, xs: tuple, raised = false) =
     echo line
 
 proc succeeded(n: int) =
+  # TODO: Maybe improve output in case of success? What extras can we write?
   stdout.styledWriteLine(styleBright, fgCyan, prefix, "+++ ", resetStyle,
-      &"OK, passed {n} tests.")
+      &"OK, passed {n} quick tests.")
 
+# TODO: functions should not need to return bool, as long as they return other
+# properties. So we need a Property type, defined as a function that returns a
+# bool or another property. Or, this is the Testable type and the Property
+# type is something else.
+
+# TODO: too much repeated code in quick procs. We might need a single one or
+# two, the current design doesn't scale well.
 proc quick*[T](f: T -> bool): bool =
   template test(i: int, x: T): bool =
     try:
       f(x)
     except:
+      # TODO: should this be `finally:`?
       failed(i, (x,), raised = true)
       raise
 
@@ -50,6 +109,7 @@ proc quick*[S, T](f: (S, T) -> bool): bool =
     try:
       f(x, y)
     except:
+      # TODO: should this be `finally:`?
       failed(i, (x, y), raised = true)
       raise
 
@@ -71,6 +131,7 @@ proc quick*[R, S, T](f: (R, S, T) -> bool): bool =
     try:
       f(x, y, z)
     except:
+      # TODO: should this be `finally:`?
       failed(i, (x, y, z), raised = true)
       raise
 
--- src/quickcheck/arbitrary.nim
+++ src/quickcheck/arbitrary.nim
@@ -5,8 +5,16 @@ import
   tables,
   typetraits
 
+# TODO: check how quickcheck defines arbitrary for typical types in Haskell
 # TODO: define and use coarbitrary?
 
+# TODO: support nestedness and generic types. Recursive types requires some
+# type of depth analysis.
+
+# TODO: ensure good char and string arbitraries.
+
+# TODO: transform arbitrary into a proc/func, not a template. We want the
+# user to overload it if she wants.
 template arbitrary*(T: typedesc): auto =
   var result: T
   when compiles(rand(T)):
@@ -20,7 +28,7 @@ template arbitrary*(T: typedesc): auto =
     for _ in 0'u8..<arbitrary(uint8):
       result.add arbitrary(elementType(result))
   else:
-    raise newException(ValueError, &"could not generate arbitrary {T}, please open an issue at https://github.com/schneiderfelipe/quickcheck/issues/new")
+    raise newException(ValueError, &"could not generate arbitrary {$T}, please open an issue at https://github.com/schneiderfelipe/quickcheck/issues/new")
   result
 
 template arbitrary*(T: typedesc[array]): auto =
@@ -29,6 +37,8 @@ template arbitrary*(T: typedesc[array]): auto =
     result[i] = arbitrary(elementType(result))
   result
 
+# TODO: generalize to functions of arbitrary inputs?
+# TODO: generalize to functions of arbitrary outputs?
 proc arbitrary*[R, S](T: typedesc[R -> S]): auto =
   var cache: Table[R, S]
   let result = proc(x: R): S =
