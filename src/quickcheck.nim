362467882814c48e80a0cc46e81a7c2ed91bccd2
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-27 13:07:02 -0300

    Run many tests and don't support negating satisfy

--- src/quickcheck.nim
+++ src/quickcheck.nim
@@ -1,10 +1,11 @@
 import
   random,
+  strformat,
   sugar
 
 
 type
-  Arbitrary* {.explain.} = concept a
+  Arbitrary* = concept a
     ## An `Arbitrary` type is any type `T` for which `some(T)` returns an
     ## arbitrary value of type `T`.
     some(typeof(a)) is typeof(a)
@@ -45,17 +46,22 @@ proc evaluate[T](p: Property[T]): bool =
 
 
 type
-  Testable* {.explain.} = concept t
+  Testable* = concept t
     ## A `Testable` is any type that can be converted to a `Property`
     property(t) is Property
 
-proc satisfy*(n: int, t: Testable): bool =
+proc satisfy*(n: Natural, t: Testable): bool =
   ## Convert a `Testable` into a `Property` and determine whether the
   ## `Property` is satisfied.
   let p = property t
-  evaluate p
+  for i in 1..n:
+    if not evaluate p:
+      echo &"Failed after {i} tests."
+      return false
+  echo &"Passed {n} tests."
+  return true
 
 proc satisfy*(t: Testable): bool =
   ## Convert a `Testable` into a `Property` and determine whether the
   ## `Property` is satisfied. By default, 100 tests are attempted.
-  satisfy(n = 100, t)
+  satisfy(100, t)
--- tests/test_basics.nim
+++ tests/test_basics.nim
@@ -30,30 +30,21 @@ suite "Testable types":
 suite "Basic usage examples":
   test "satify accepts booleans":
     check satisfy true
-    check not satisfy false
 
   test "satify accepts zero-argument functions":
     check satisfy () => true
-    check not satisfy () => false
 
   test "satify accepts single-argument functions":
     check satisfy (x: int) => true
-    check not satisfy (x: int) => false
 
   test "satify accepts properties":
     check satisfy property(true)
-    check not satisfy property(false)
     check satisfy property(() => true)
-    check not satisfy property(() => false)
     check satisfy property((x: int) => true)
-    check not satisfy property((x: int) => false)
 
 suite "Using options":
   test "satify accepts an integer parameter":
     check satisfy(10) do: true
-    check not (satisfy(10) do: false)
     check satisfy(10) do () -> bool: true
-    check not satisfy(10, () => false)
     check satisfy(10) do (x: int) -> bool:
       true
-    check not satisfy(10, proc(x: int): bool = false)
