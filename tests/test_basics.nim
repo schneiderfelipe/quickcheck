e16fc792a618dc92f0319bc48093a0182c5d019c
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-27 13:32:05 -0300

    Initial redesign ready

--- tests/test_basics.nim
+++ tests/test_basics.nim
@@ -28,22 +28,22 @@ suite "Testable types":
       check property((n: int) => b) is Testable
 
 suite "Basic usage examples":
-  test "satify accepts booleans":
+  test "satisfy accepts booleans":
     check satisfy true
 
-  test "satify accepts zero-argument functions":
+  test "satisfy accepts zero-argument functions":
     check satisfy () => true
 
-  test "satify accepts single-argument functions":
+  test "satisfy accepts single-argument functions":
     check satisfy (x: int) => true
 
-  test "satify accepts properties":
+  test "satisfy accepts properties":
     check satisfy property(true)
     check satisfy property(() => true)
     check satisfy property((x: int) => true)
 
 suite "Using options":
-  test "satify accepts an integer parameter":
+  test "satisfy accepts an integer parameter":
     check satisfy(10) do: true
     check satisfy(10) do () -> bool: true
     check satisfy(10) do (x: int) -> bool:
