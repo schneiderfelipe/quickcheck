ddd92aeed1ae128fd2af12698d73c9917aca046c
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-06-27 12:21:22 -0300

    Allow testing zero- and one-argument functions

--- src/quickcheck.nim
+++ src/quickcheck.nim
@@ -1,3 +1,41 @@
-func satisfy*(x: bool): bool =
-  ## Determine whether a `bool` is `true`.
-  x
+import sugar
+
+type
+  Property[T] = object
+    ## A `Property` is a representation of a statement about some data.
+    test: T -> bool
+
+func property*(b: bool): auto =
+  ## A `Property` without any input, constructed from a `bool`.
+  Property[void](test: () => b)
+
+func property*(f: () -> bool): auto =
+  ## A `Property` without any input.
+  Property[void](test: f)
+
+func property*[T](f: T -> bool): auto =
+  ## A `Property` with a single input.
+  Property[T](test: f)
+
+func property*(p: Property): auto =
+  ## Return the given `Property`.
+  p
+
+proc evaluate(p: Property[void]): bool =
+  ## Evaluate a property without any input once.
+  p.test()
+
+proc evaluate[T](p: Property[T]): bool =
+  ## Evaluate a property with a single input once.
+  p.test(1)
+
+type
+  Testable* = concept a
+    ## A `Testable` is any type that can be converted to a `Property`
+    property(a) is Property
+
+proc satisfy*(t: Testable): bool =
+  ## Convert a `Testable` into a `Property` and determine whether the
+  ## `Property` is satisfied.
+  let p = property t
+  evaluate p
--- tests/test_basics.nim
+++ tests/test_basics.nim
@@ -1,6 +1,46 @@
 import unittest, quickcheck
+import
+  sugar,
+  unittest
+
+import quickcheck
+
+suite "Testable types":
+  test "Booleans are testable":
+    for b in [true, false]:
+      check b is Testable
+
+  test "Zero-argument functions returning booleans are testable":
+    for b in [true, false]:
+      check (() => b) is Testable
+
+  test "Single-argument functions returning booleans are testable":
+    for b in [true, false]:
+      check ((n: int) => b) is Testable
+
+  test "Properties are testable":
+    for b in [true, false]:
+      check property(b) is Testable
+      check property(() => b) is Testable
+      check property((n: int) => b) is Testable
 
 suite "Basic usage examples":
   test "satify accepts booleans":
     check satisfy true
     check not satisfy false
+
+  test "satify accepts zero-argument functions":
+    check satisfy () => true
+    check not satisfy () => false
+
+  test "satify accepts single-argument functions":
+    check satisfy (x: int) => true
+    check not satisfy (x: int) => false
+
+  test "satify accepts properties":
+    check satisfy property(true)
+    check not satisfy property(false)
+    check satisfy property(() => true)
+    check not satisfy property(() => false)
+    check satisfy property((x: int) => true)
+    check not satisfy property((x: int) => false)
