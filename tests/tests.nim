b59dda76c17eb2b38ccd955961f5cafa40f29b57
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-07-08 18:46:55 -0300

    Rewrite basic functionality from scratch

--- tests/tests.nim
+++ tests/tests.nim
@@ -1,6 +1,31 @@
 import unittest, quickcheck
 
-suite "Basic functionality":
-  test "can test a trivial property":
-    check satisfy do (x: int) -> bool:
+
+# https://github.com/nim-lang/Nim/issues/18466
+template fail(conditions: auto): auto =
+  ## A convenience meaning the exact opposite of `check`.
+  check(not (conditions))
+
+
+suite "Trivial functionality":
+  test "pass a successful trivial property":
+    check satisfy do () -> bool:
       true
+
+  test "don't pass a failing trivial property":
+    fail satisfy do () -> bool:
+      false
+
+
+suite "Simple properties with parameters":
+  test "pass a simple property with an ignored parameter":
+    check satisfy do (_: int) -> bool:
+      true
+  
+  test "pass a simple property with a used parameter":
+    check satisfy do (n: range[0..50]) -> bool:
+      0 <= n and n <= 50
+
+  test "pass a simple property with more than one parameter":
+    check satisfy do (n: int8) -> bool:
+      2 * int(n) mod 2 == 0  # convert to avoid overflow
